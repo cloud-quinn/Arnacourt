209ae7a698e3159686d4cc53542583b15fd66cb3
Kaylee Eluvian <kaylee.eluvian@icloud.com>
2015-10-01 16:22:12 +0100

    Created the Arnak

--- code.js
+++ code.js
@@ -2,17 +2,19 @@ var fieldHeight = 0;
 var fieldWidth = 0;
 var context = null;
 var actors = {};
-var collectables = 8;
+var collectables = 2;
 var score = 0;
 var scoreArea = 25;
 var level = 1;
 var background = '#559955';
 var altBackground = '#555599';
+var lives = 2;
 
 var library = {
   "select": {"Volume":{"Sustain":0.1,"Decay":0.15,"Punch":0.55}},
   "long": {"Volume":{"Sustain":0.1,"Decay":0.5,"Punch":1}},
-  "apple":{"Generator":{"Func":"sine","A":0,"B":0},"Volume":{"Attack":0,"Sustain":0.07,"Decay":0.19,"Punch":0.26,"Master":0.32},"Filter":{"LP":1,"LPSlide":-0.45,"LPResonance":0},"Frequency":{"Min":983,"Max":1428,"Slide":0.37,"DeltaSlide":0.62,"RepeatSpeed":1.57,"Start":828,"ChangeAmount":1,"ChangeSpeed":0.24},"Vibrato":{"Depth":0.29,"DepthSlide":-0.17,"Frequency":34.01,"FrequencySlide":-0.24},"Phaser":{"Offset":0.01,"Sweep":-0.01}}
+  "apple":{"Generator":{"Func":"sine","A":0,"B":0},"Volume":{"Attack":0,"Sustain":0.07,"Decay":0.19,"Punch":0.26,"Master":0.32},"Filter":{"LP":1,"LPSlide":-0.45,"LPResonance":0},"Frequency":{"Min":983,"Max":1428,"Slide":0.37,"DeltaSlide":0.62,"RepeatSpeed":1.57,"Start":828,"ChangeAmount":1,"ChangeSpeed":0.24},"Vibrato":{"Depth":0.29,"DepthSlide":-0.17,"Frequency":34.01,"FrequencySlide":-0.24},"Phaser":{"Offset":0.01,"Sweep":-0.01}},
+  "death":{"Frequency":{"Start":30,"Min":122,"Max":973,"DeltaSlide":0.03,"Slide":-1},"Volume":{"Master":0.46,"Attack":0,"Sustain":0.12,"Punch":2.95,"Decay":0.15},"Generator":{"Func":"saw","ASlide":-0.18,"BSlide":0.65},"Vibrato":{"Depth":0.85,"DepthSlide":-0.49,"Frequency":13.01,"FrequencySlide":-0.67},"Phaser":{"Offset":-0.59,"Sweep":0.48}}
 };
 
 var sfx = jsfx.Sounds(library);
@@ -25,7 +27,7 @@ $(document).ready(function(){
 });
 
 function redraw(){
-  checkCollisions(context);
+  checkCollisions(context, actors);
   checkEndOfLevel(context, actors);
   clearField(context); 
   drawActors(context,actors);
@@ -67,15 +69,44 @@ function checkEndOfLevel(field, actors){
   }, 300);
 };
 
-function advanceLevel(actors){
-  actors.player.x = 0;
-  actors.player.y = fieldHeight - scoreArea;
+function resetLevel(field, actors)
+{
   for(var a in actors){
     var actor = actors[a];
-    if (actor.value > 0) actor.hide = false;
+    if (actor.value > 0)
+      actor.hide = false;
   }
 
+  actors.player.x = 0;
+  actors.player.y = fieldHeight - scoreArea-15;
+  actors.arnak.x = fieldWidth-15;
+  actors.arnak.y = 0 
+}
+
+function advanceLevel(actors){
+  for(var a in actors){
+    var actor = actors[a];
+    if (actor.value > 0)
+      actor.hide = false;
+
+    var x = Math.floor(Math.random() * fieldWidth - 10);
+    var y = Math.floor(Math.random() * fieldHeight - 10);
+    actor.x = x;
+    actor.y = y;
+
+  }
+
+  actors.player.x = 0;
+  actors.player.y = fieldHeight - scoreArea;
+  actors.arnak.x = fieldWidth-15;
+  actors.arnak.y = 0
   level++;
+  if (collectables < 10){
+    var x = Math.floor(Math.random() * fieldWidth - 10);
+    var y = Math.floor(Math.random() * fieldHeight - 10);
+    actors['apple' + collectables] = {hide:false, value:10, harm: 0, x:x, y:y, width:10, height:10, speed:0, data:{}, tick: function(){}, draw: drawApple};
+    collectables++;
+  } 
 };
 
 function updateScore(field, score){
@@ -84,11 +115,12 @@ function updateScore(field, score){
   field.fillStyle = '#000000';
   field.font = '10px Verdana';
   field.fillText('Score: ' + score, 5, fieldHeight + 10);
-  field.fillText('Day: ' + level, 5, fieldHeight + 20);
+  field.fillText('Day:   ' + level, 5, fieldHeight + 20);
+  field.fillText('Lives: ' + lives, fieldWidth/2, fieldHeight + 10);
   field.closePath();
 };
 
-function checkCollisions(field){
+function checkCollisions(field, actors){
   // Has the player collided with anything?
   var player = actors.player;
   for(var a in actors){
@@ -125,16 +157,26 @@ function checkCollisions(field){
         actor.hide = true;
         sfx.apple();
       }
-      else console.log('Collision');
-    }
+      else if (actor.harm > 0)
+      {
+        sfx.death();
+        lives--;
+        if (lives ===0){
+         alert('Game over.');
+         return; 
+       }
 
-  }
+       resetLevel(field, actors);
+     }
+   }
+
+ }
 
 };
 
 function createActors(){
   actors.player = {hide: false, name: 'player', value:0, harm: 0, x:0, y:fieldHeight-scoreArea,width:15,height:15,speed:10,data:{lastX:0, lastY:0}, tick: function(field){}, draw: drawPlayer};
-  
+  actors.arnak  = {hide: false, name: 'arnak', value:0, harm: 1, x:fieldWidth, y:0,width:15,height:15,speed:10,data:{lastX:0, lastY:0}, tick: function(field){}, draw: drawArnak};
   for(var c = 0; c < collectables; c++){
     var x = Math.floor(Math.random() * fieldWidth - 10);
     var y = Math.floor(Math.random() * fieldHeight - 10);
@@ -277,5 +319,36 @@ function drawPlayer(field)
   field.fillStyle = '#CCBBBB';
   field.stroke();
   field.fill();
+  field.closePath();
+};
+
+function drawArnak(field){
+  // Draw body
+  field.beginPath();
+  field.moveTo(this.x + this.width*(6/12), this.y);
+  field.lineTo(this.x + this.width*(8/12), this.y + this.height*(2/12));
+  field.lineTo(this.x + this.width*(9/12), this.y + this.height*(6/12));
+  field.lineTo(this.x + this.width*(10/12), this.y + this.height*(8/12));
+  field.lineTo(this.x + this.width*(12/12), this.y + this.height*(12/12));
+  field.lineTo(this.x, this.y + this.height*(12/12));
+  field.lineTo(this.x + this.width*(2/12), this.y + this.height*(8/12));
+  field.lineTo(this.x + this.width*(3/12), this.y + this.height*(6/12));
+  field.lineTo(this.x + this.width*(4/12), this.y + this.height*(2/12));
+  field.lineTo(this.x + this.width*(6/12), this.y);
+  field.fillStyle = '#CCCCCC';
+  field.fill();
+  field.stroke();
+  field.closePath();
+
+  // Draw head
+  field.beginPath();
+  field.moveTo(this.x + this.width*(4/12), this.y + this.height*(2/12));
+  field.lineTo(this.x + this.width*(2/12), this.y + this.height*(3/12));
+  field.lineTo(this.x + this.width*(2/12), this.y + this.height*(5/12));
+  field.lineTo(this.x + this.width*(1/12), this.y + this.height*(6/12));
+  field.lineTo(this.x + this.width*(3/12), this.y + this.height*(6/12));
+  field.fillStyle = '#BBBBBB';
+  field.fill();
+  field.stroke();
   field.closePath();
 };
